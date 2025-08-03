4b46f7fcae62b20220a609c4500bea8fc12cc8c2
Fares Anwar <faresaof20@gmail.com>
2025-08-04 00:47:48 +0300

    feate(auth): create register and login api

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -1,27 +1,39 @@
 import { hashSync } from "bcrypt";
 import User from "../../DB/models/user.model.js"
+import bcrypt from "bcryptjs";
 
+// Register
 export const register = async(req, res)=>{
     try {
         const {email, password, phone, userName, confirmedPassword}=req.body;
 
-        if(password !== confirmedPassword){
-            return res.status(201).json({success:false, massage:"passwords must match!"})
-        }
+        if(password !== confirmedPassword) return res.status(500).json({success:false, message:"passwords not matched!"})
 
-        const hash =hashSync(password,8)
-        
-        const user = await User.create({email, password:hash, phone, userName})
+        const hashPassword=  hashSync(password, 8)
 
-        return res.status(201).json({success:true, results:{user}})
+        const user =await User.create({email, password:hashPassword, phone, userName})
+
+        return res.status(200).json({success:true, results:{user}})
     } catch (error) {
         return res.status(500).json({success:false, stack:error.stack})
     }
 }
 
+// Login
 export const login = async(req, res)=>{
     try {
-        return res.status(201).json({success:true, results})
+        // Check user
+        const {email, password}=req.body
+
+        const user = await User.findOne({email})
+
+        if(!user) return res.status(404).json({success:false, message:"Invalid Email!"})
+
+        // Check password
+        const matchPassword=bcrypt.compareSync(password, user.password)
+
+        if(!matchPassword) if(!user) return res.status(404).json({success:false, message:"Invalid Password!"})
+        return res.status(200).json({success:true, results:{user}})
     } catch (error) {
         return res.status(500).json({success:false, stack:error.stack})
     }
